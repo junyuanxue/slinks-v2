ad94049064861bd4efff1fb3388d07e147bc7b45
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-26 11:46:37 +0100

    set up env var

--- test/unit/slinksService.spec.js
+++ test/unit/slinksService.spec.js
@@ -1,12 +1,36 @@
 describe('SlinksService', function() {
   beforeEach(module('slinksApp'));
 
-  var SlinksService, httpBackend
+  var SlinksService, httpBackend;
+
+  var slinksData = {
+    matches: [
+      {
+        text: "<http://slack.com/>",
+        previous: {
+          text: "<http://expressjs.com/>"
+        },
+        previous_2: {
+          text: "<https://mochajs.org/>"
+        },
+        next: {
+          text: "This is not a link!"
+        },
+        next_2: {
+          text: "<https://www.mongodb.org/>"
+        }
+      }
+    ]
+  }
 
   beforeEach(inject(function(_SlinksService_, $httpBackend) {
     SlinksService = _SlinksService_;
     httpBackend = $httpBackend;
   }));
 
-  
+  it('fetches a list of links from Slack API', function() {
+    var token = ENV['SLACK_API_TOKEN'];
+    httpBackend.expectGet("https://slack.com/api/search.messages?token=" + token + "&query=http:\/\/&pretty=1");
+  });
+
 });
