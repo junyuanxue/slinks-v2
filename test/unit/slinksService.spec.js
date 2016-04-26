9445f7fc23c0f776367537c2c35c794b175841e8
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-26 11:16:27 +0100

    writing test for service

---
+++ test/unit/slinksService.spec.js
@@ -0,0 +1,12 @@
+describe('SlinksService', function() {
+  beforeEach(module('slinksApp'));
+
+  var SlinksService, httpBackend
+
+  beforeEach(inject(function(_SlinksService_, $httpBackend) {
+    SlinksService = _SlinksService_;
+    httpBackend = $httpBackend;
+  }));
+
+  
+});
