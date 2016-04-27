2cbca87828adff0cd05e036b44ee07b07502b33f
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-27 11:48:05 +0100

    created database and table

--- app/models/database.js
+++ app/models/database.js
@@ -1,6 +1,7 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:8080/slinks';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost/slinks';
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE slinks(id SERIAL PRIMARY KEY, url )')
+var query = client.query('CREATE TABLE slinks(id SERIAL PRIMARY KEY, url TEXT not null, starred BOOLEAN)');
+query.on('end', function() { client.end(); });
