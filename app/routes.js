f8e60e6b3a23d1a6d1e6ac19228a0bc2c6e5137f
Claudia Green <claudiagreen@Claudias-MacBook-Pro-3.local>
2016-04-27 12:32:20 +0100

    psql set up, attempting to put data into the table from a post request. Currently not working.

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,44 @@
 module.exports = function(app) {
 
+  var path = require('path');
+  var pg = require('pg');
+  var connectionString = 'postgres://localhost/slinks';
   var request = require('request');
   var token = process.env.SLACK_API_TOKEN;
 
   app.get('/', function(req, res) {
-    var path = require('path');
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
   });
+
+  app.post('/slinks', function (req, res) {
+    var results = [];
+    console.log('hi!!!');
+    console.log(req);
+    console.log('**********************');
+    console.log(req.data.url);
+
+    var data = {url: "dummy data", starred: false};
+
+    pg.connect(connectionString, function(err, client, done) {
+      if(err) {
+        done();
+        return res.status(500).json({ success: false, data: err});
+      }
+
+      client.query("INSERT INTO slinks(url, starred) values($1, $2)", [data.url, data.starred]);
+      var query = client.query("SELECT * FROM slinks ORDER BY id ASC");
+      query.on('row', function(row) {
+        results.push(row);
+      });
+      query.on('end', function() {
+        done();
+        return res.json(results);
+      });
+    });
+
+  });
+
+
 
   app.get('/slinks', function(req, res) {
     _requestToSlack().then(function(slinksData) {
@@ -34,4 +66,6 @@ module.exports = function(app) {
       });
     });
   };
+
+
 };
